79400803ee4ca2c257768b2f3e765e89546df05a
Raunak <raunakgoshami@gmail.com>
2024-06-13 19:36:42 +0530

    module exports

--- hello.js
+++ hello.js
@@ -1,2 +1,6 @@
 //dom manipulations are not available here
-console.log("hey from node.js 2");
+const { add, sub, mul, div } = require("./math");
+console.log(add(2, 3));
+console.log(sub(2, 3));
+console.log(mul(2, 3));
+console.log(div(4, 2));
---
+++ math.js
@@ -0,0 +1,9 @@
+exports.add = (a, b) => {
+  return a + b;
+};
+
+exports.sub = (a, b) => a - b;
+
+exports.mul = (a, b) => a * b;
+
+exports.div = (a, b) => a / b;
