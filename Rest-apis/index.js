20d63a736348a025a30e830e598ee5e6a413a26c
Raunak <raunakgoshami@gmail.com>
2024-06-16 22:01:10 +0530

    handling status codes

--- Rest-apis/index.js
+++ Rest-apis/index.js
@@ -16,6 +16,7 @@ app.get("/get", (req, resp) => {
 });
 
 app.get("/api/get", (req, resp) => {
+  resp.setHeader("x-Powered-By", "RG-Server");
   return resp.json(data);
 });
 
@@ -25,12 +26,25 @@ app.get("/api/get/:id", (req, resp) => {
     if (user.id == newId) return user;
   });
   console.log(newId);
+  if (filteredData.length === 0) {
+    return resp.status(404).json({ Error: "No user with such id exists" });
+  }
+  console.log("here is filtered data", filteredData);
   return resp.json(filteredData);
 });
 
 app.post("/api/post", (req, res) => {
   console.log(req.body);
   const addData = req.body;
+  if (
+    !addData.first_name ||
+    !addData.last_name ||
+    !addData.email ||
+    !addData.gender ||
+    !addData.ip_address
+  ) {
+    return res.status(400).json(`Bad input`);
+  }
   JSON.stringify(data.push({ ...addData, id: data.length + 1 }));
   console.log("old data ", data);
   console.log("we have the new data", data);
@@ -38,7 +52,8 @@ app.post("/api/post", (req, res) => {
   fs.writeFile("./MOCK_DATA.json", JSON.stringify(data), (err) => {
     console.log(err);
   });
-  return res.json(`successfully added new ID: ${data.length}`);
+
+  return res.status(201).json(`successfully added new ID: ${data.length}`);
 });
 
 app.listen(PORT, (req, res) => {
